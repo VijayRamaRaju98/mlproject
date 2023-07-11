309c68c578b4dd2fd13f7b5a61825e5c702a6ddd
Vijaya Rama Raju Uppalapati <uppalapativijay98@gmail.com>
2023-07-11 08:03:32 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,10 +3,11 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-from src.components import model_trainer
+from src.components.model_trainer import ModelTrainer
 
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
+from src.components.model_trainer import ModelTrainer
 
 
 from src.utils import save_object
@@ -55,4 +56,7 @@ if __name__=="__main__":
 
     data_transformation = DataTransformation()
     
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr,_= data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
